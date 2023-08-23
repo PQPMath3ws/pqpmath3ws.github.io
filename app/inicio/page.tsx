bcc46155a2fee18676b47969c7e4fee95ef9d06b
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-08-23 09:43:37 -0300

    Correção da área de experiências e atualização de projeto

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -66,7 +66,7 @@ export default function PaginaDeInicio() {
   const birthDate: Date = new Date(1998, 10, 24, 11, 30, 0, 0)
   const todayDate: Date = new Date()
 
-  const monthDiff: number = todayDate.getTime() - birthDate.getTime()
+  const monthDiff: number = todayDate.getTime() - birthDate.getTime() + 3600000
   const realAgeDateDiff: Date = new Date(monthDiff)
 
   const birthDateAndTodayDateResult: number = Math.abs(realAgeDateDiff.getFullYear() - 1970)
@@ -82,6 +82,7 @@ export default function PaginaDeInicio() {
             <p>Deixe-me apresentar-me por um momento: Me chamo <b className="text-red-800"><i>Mathews Martins</i></b>, tenho <b className="text-red-800"><i>{birthDateAndTodayDateResult} anos</i></b> e moro atualmente em <b className="text-red-800"><i>Montes Claros, Minas Gerais</i></b>.</p>
             <p>Estudo programação desde os meus <b className="text-red-800"><i>13 anos</i></b> de idade. Desde então, comecei pequenos projetos pessoais e fui escalonando meus conhecimentos e práticas na área.</p>
             <p>Aos <b className="text-red-800"><i>18 anos</i></b> de idade, comecei a fazer faculdade de <b className="text-red-800"><i>Sistemas de Informação</i></b>. Aos <b className="text-red-800"><i>20 anos</i></b>, realizei meu primeiro estágio na área de <i className="text-[#000000]">Desenvolvimento Mobile (Android e iOS)</i> e também meu primeiro estágio em <i className="text-[#000000]">Desenvolvimento Web.</i></p>
+            <p>Já participei/colaborei em alguns projetos de forma <b className="text-red-800"><i>voluntária</i></b>, projetos esses que dei o meu melhor para fazer acontecer e dar certo.</p>
             <p>E desde então, entre idas e vindas, estou há <b className="text-red-800"><i>5 anos</i></b> como programador/desenvolvedor no mercado. Participei também de alguns projetos colaborativos durante esse tempo.</p>
             <p>E esse sou euzinho:</p>
             <Image src="./images/me.png" alt="Uma imagem do autor do website, que é branco, dos olhos castanhos, cabelo baixo, e vestido com camisa social branca." width="1000" height="1000">
@@ -93,7 +94,7 @@ export default function PaginaDeInicio() {
           <p className={`${pressStart2P.className} bg-[#202020] text-[#DEDEDE] text-[30px] px-[14px] py-[14px] rounded-xl`}>Contribuições</p>
           <div className={`w-[80%] ${hammersmithOne.className} text-center text-[20px] flex flex-col gap-5 text-[#202020]`}>
             <div className="p-5 mb-4 border border-gray-100 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-              <time className="text-lg text-gray-900 dark:text-white">Janeiro de 2023 - Atualmente</time>
+              <time className="text-lg text-gray-900 dark:text-white">Janeiro de 2023 - Agosto de 2023</time>
               <div className="mt-3">
                 <Link href="https://discord.gg/sectorcrp" className={`items-center block p-3 sm:flex flex-col hover:bg-gray-100 dark:hover:bg-gray-700 gap-6 text-[#DEDEDE] text-[24px] ${hammersmithOne.className}`} rel="noreferrer" target="_blank">
                   <Image className="w-[40%] h-[40%] mb-3 mr-3 rounded-full sm:mb-0" src="./images/sectorc.webp" alt="Jese Leos image" width="500" height="500"></Image>
@@ -147,7 +148,22 @@ export default function PaginaDeInicio() {
                   Abril de 2021 - Junho de 2023
                 </time>
                 <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Manutenção e criação de novas features de sistema legacy construído na Stack MEAN.js (MongoDB, Express.js, Angular.js & Node.js); Manutenção e criação de novas features no aplicativo Android Java Nativo (com Angular.js no webview); Adaptação do sistema para nova tecnologia (Django / Python & Django) no web com Docker; Adaptação do sistema mobile para nova tecnologia (React Native & RealmDB);
+                  - Responsável por dar manutenção e desenvolver novas funções/recursos - tanto no sistema web / api da empresa, usando MEAN.js (MongoDB, Express.js, Angular.js e Node.js), em um ambiente com containers e CI/CD para os servidores dos clientes - quanto na aplicação android nativa, usando Java no aplicativo, e Angular.js na parte webview (página web local).
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Uso do GitHub para versionamento da aplicação e distribuição (CI/CD) da aplicação em todos os clientes.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Responsável por realizar manutenção nos servidores dos clientes e em seu ambiente configurado (manutenção e configuração do ambiente do docker, para antigos e novos clientes, e manutenção no firewall), e atualização do processo de CI/CD do sistema (tanto no GitHub, quanto nos servidores).
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Responsável por iniciar e começar a migração do sistema da empresa para novas tecnologias disponíveis no mercado, a fim de implementar boas práticas de desenvolvimento da aplicação web e mobile, e reduzir as limitações existentes no cenário atual, por utilizar tecnologias antigas anteriormente. As tecnologias selecionadas foram: Django (com Python 3) + PostgreSql + HTML5 + CSS3 + JavaScript para a parte Web, e React Native (com JavaScript) + RealmDB para a parte Android.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Realizado estudo e prática de DevOps para manutenção dos ambientes.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Liderança do time de desenvolvimento durante parte de minha experiência na empresa.
                 </p>
               </li>
               <li className={`mb-10 ml-6 ${hammersmithOne.className}`}>
@@ -161,7 +177,22 @@ export default function PaginaDeInicio() {
                   Novembro de 2020 - Abril de 2022
                 </time>
                 <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Desenvolvimento de aplicação mobile (Android e iOS) em Flutter (Dart), consultando API pública do WordPress; Adaptação e correção de jogos educativos no Unity 2D (C#) e no Flutter (Dart).
+                  - Recebimento, colaboração e liderança no desenvolvimento da aplicação da startup.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Utilização da framework Flutter (com a linguagem Dart) para o desenvolvimento do aplicativo mobile.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Utilização do Unity 2D (com a linguagem C#) para desenvolvimento de alguns jogos educacionais relacionados ao objetivo do projeto.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Utilização do framework Flutter (com a linguagem Dart) para desenvolvimento de alguns jogos educacionais relacionados ao objetivo do projeto.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Utilização da metodologia ágil Scrum durante o ciclo de desenvolvimento da aplicação.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Entendimento e aplicação da consulta da Rest API pública do WordPress pelo aplicativo.
                 </p>
               </li>
               <li className={`mb-10 ml-6 ${hammersmithOne.className}`}>
@@ -175,7 +206,19 @@ export default function PaginaDeInicio() {
                   Novembro de 2019 - Dezembro de 2020
                 </time>
                 <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Desenvolvimento de página principal da empresa (PHP Puro); Desenvolvimento de sistema OS (Ordem de Serviço) em CodeIgniter 4 (PHP + MySQL + Bootstrap 4)
+                  - Responsável por desenvolver o site principal da empresa para apresentação dos serviços para possíveis clientes, utilizando PHP Puro, HTML, CSS, JavaScript e Bootstrap 4 (para o front-end).
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Responsável por desenvolver o sistema de OS (Ordem de Serviço) da empresa para troca de documentos entre clientes e funcionários da empresa, utilizando CodeIgniter 4 (em PHP), HTML, CSS, JavaScript e Bootstrap 4 (para o front-end).
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Responsável por levantar tecnologias, requisitos funcionais e não-funcionais para o desenvolvimento de site da empresa e sistema de OS da mesma.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Utilizado a metodologia ágil XP durante o ciclo de desenvolvimento de ambos os sistemas.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Realizado estudo e prática de PHP durante o estágio realizado na empresa.
                 </p>
               </li>
               <li className={`ml-6 ${hammersmithOne.className}`}>
@@ -189,7 +232,16 @@ export default function PaginaDeInicio() {
                   Maio de 2019 - Agosto de 2019
                 </time>
                 <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                  Desenvolvimento e manutenção de aplicação mobile (Android & iOS) usando Xamarin.Forms (C# + XAML + MySQL), com servidor (Windows Server) próprio; Manutenção de website WordPress (PHP 7 + MySQL).
+                  - Responsável por dar continuidade ao desenvolvimento da aplicação da empresa, onde foram utilizadas as tecnologias: Xamarin.Forms (com a linguagem C#, para o back-end e XAML, para o front-end da aplicação) e SQLite (dentro da aplicação, para armazenar informações e estados do aplicativo).
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Utilização e prática da metodologia ágil Scrum no ciclo de vida útil do desenvolvimento da aplicação.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Resposável por realizar testes de integração da API com a aplicação mobile, para identificar possíveis erros/alterações necessárias a realizar.
+                </p>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400 mt-4">
+                  - Realizado estudo e prática de Xamarin.Forms (com C# e XAML), SQLite e Windows Server durante o estágio realizado na empresa.
                 </p>
               </li>
             </ol>
